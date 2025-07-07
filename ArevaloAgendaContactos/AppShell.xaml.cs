95ea2b86da22dd57d77d39f35ee58f6290624aef
laboratorio <laboratorio@UPOAULA10704>
2025-07-07 07:14:31 -0500

    Agregar archivos de proyecto.

---
+++ ArevaloAgendaContactos/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace ArevaloAgendaContactos
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ ArevaloAgendaContactos/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ArevaloAgendaContactos;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
